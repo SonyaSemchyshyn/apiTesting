2d3338fed1671f907d77bb7570b8a9eccd881697
SonyaSemchyshyn <96415772+SonyaSemchyshyn@users.noreply.github.com>
2024-03-11 19:56:40 +0200

    change api endpoint

--- test/test.js
+++ test/test.js
@@ -1,14 +1,12 @@
 import { expect } from 'expect';
-import chai from 'chai';
+import chai from "chai";
 import chaiHttp from 'chai-http';
-
 chai.use(chaiHttp);
 
-
-describe('Array', async function () {
+describe('', async function () {
     it('GET reguest', function () {
-        let res = chai.request('https://fakerestapi.azurewebsites.net')
-        .get('/api/v1/Activities');
-         expect(res).to.have.status(200);
+      let token = 'dxyo1YtXgQIAHA6FWjTXom7tdkwST4JJ4LfhP0DZtNgh';
+      let res = chai.request('https://rbaskets.in').get('/firstMethod').set({ "Authorization": `Bearer ${token}` })
+      expect(res).to.have.status(200);    
     });
   });
